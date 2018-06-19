c95717343824cc8b2402ef3e4c2e1a5453df4255
Tim Peeters <tim.peeters@xplore.be>
2018-06-19 10:44:26 +0200

    Force the Spring Boot Configuration Processor to generate autocomplete metadata for nested Tomcat, Jetty and Undertow.

--- src/main/java/com/github/timpeeters/boot/actuate/autoconfigure/ActuatorServerProperties.java
+++ src/main/java/com/github/timpeeters/boot/actuate/autoconfigure/ActuatorServerProperties.java
@@ -5,4 +5,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("management.server")
 public class ActuatorServerProperties extends ServerProperties {
+    @ConfigurationProperties(prefix = "management.server.jetty")
+    @Override
+    public Jetty getJetty() {
+        return super.getJetty();
+    }
+
+    @ConfigurationProperties(prefix = "management.server.tomcat")
+    @Override
+    public Tomcat getTomcat() {
+        return super.getTomcat();
+    }
+
+    @ConfigurationProperties(prefix = "management.server.undertow")
+    @Override
+    public Undertow getUndertow() {
+        return super.getUndertow();
+    }
 }
